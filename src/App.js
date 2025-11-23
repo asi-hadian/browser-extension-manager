ebb675d4f68402d7dc5dbfbb15db16d205e08dcb
asi hadian <asiyeh.hadain@gmail/com>
2025-11-23 23:03:08 +0330

    edit fetch data url

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("https://asi-hadian.github.io/browser-extension-manager/data.json")
+      .get("https://raw.githubusercontent.com/asi-hadian/browser-extension-manager/main/data.json")
       .then((response) => setData(response.data.data))
       .catch((error) => console.error("خطا در دریافت داده‌ها:", error));
   }, []);
